06adec0283eff1f320a02851e4f8b5c1be1222c8
Avikumar Talaviya <avikumar.talaviya@gmail.com>
2025-11-13 11:23:00 -0700

    sql query update

--- SQL and databases/nested_queries and joins/que2.sql
+++ SQL and databases/nested_queries and joins/que2.sql
@@ -1,7 +1,5 @@
 select s.store_id, count(i.inventory_id) as inventory_count
 from sakila.rental r
-right join sakila.inventory i using (inventory_id)
-join sakila.store s using(store_id)
-where r.rental_id is null
-group by s.store_id
-order by s.store_id;
+right join sakila.inventory i using (inventory_id) 
+join sakila.store s on i.store_id=s.store_id and i.inventory_id is null
+group by s.store_id;
